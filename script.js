a0cc714111c8603410990c830509c8e65be09f44
Moses <2000004146@bensalemsd.org>
2026-05-27 08:13:42 -0400

    Embed iframe into screenDiv with specified attributes

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const screenDiv = document.querySelector(".screen")
+const iframe = document.createElement("iframe");
 
+iframe.src = url;
+iframe.width = "100%";
+iframe.height = "100%";
+iframe.style.border = "none";
+
+screenDiv.appendChild(iframe);
