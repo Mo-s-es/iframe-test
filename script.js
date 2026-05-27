b9678a813c41b14a1b23a23b27035127b77b8506
Moses <2000004146@bensalemsd.org>
2026-05-27 08:57:46 -0400

    Refactor run_url to use variable for iframe src

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ const bar = document.getElementById("bar");
 const screen = document.querySelector(".screen");
 
 function run_url() {
+  let url = bar.value;
+  
   const iframe = document.createElement("iframe");
-
-  iframe.src = bar.value;
+  
+  iframe.src = url;
   iframe.width = "100%";
   iframe.height = "100%";
   iframe.style.border = "none";
 
   screen.appendChild(iframe);
 }
+
+bar.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    run_url();
+  }
+});
