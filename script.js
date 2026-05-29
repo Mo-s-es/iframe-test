174a7f60846f7ac215cce2f06e980ad7865d444c
Moses <2000004146@bensalemsd.org>
2026-05-29 15:58:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 console.log("Code running.");
 
+const body = document.body;
+const top = document.getElementById(".top");
 const bar = document.getElementById("bar");
+const theme = document.getElementById("theme");
 const screen = document.querySelector(".screen");
 
-function run_url() {
-  let url = bar.value;
+function run_url(url) {
+  url = bar.value;
   
   if (!url.startsWith("https://") && !url.startsWith("http://")) {
     if (url == "") {
@@ -26,10 +29,28 @@ function run_url() {
   screen.appendChild(iframe);
 };
 
-run_url()
+let url = "";
+let theme = "dark";
+
+run_url(url);
 
 bar.addEventListener("keydown", function(event) {
   if (event.key === "Enter") {
+    console.log("URL (" + url + ") entered");
     run_url();
+  };
+});
+
+theme.addEventListener("click", function() {
+  if (theme == "dark") {
+    theme = "light";
+    document.body.style.backgroundColor = #ffffff;
+    document.top.style.backgroundColor = #ffffff;
+    document.screen.style.backgroundColor = #ffffff;
+  } elif (theme == "light") {
+    theme = "dark";
+    document.body.style.backgroundColor = #000000;
+    document.top.style.backgroundColor = #000000;
+    document.screen.style.backgroundColor = #000000;
   };
 });
